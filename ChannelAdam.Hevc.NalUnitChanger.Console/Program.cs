a982d6fed614af60450a0f1c067231b2e509577f
Adam Craven <adammcraven@gmail.com>
2017-07-02 23:43:07 +1000

    Add project files.

---
+++ ChannelAdam.Hevc.NalUnitChanger.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChannelAdam.Hevc.NalUnitChanger.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
